576dd7195ee0cde160e7896cb7adaa246cdeb35f
petercharlesxp <petercharles.ca@hotmail.com>
2016-03-15 15:36:53 -0400

    using RaiseCanExecuteChanged in properties

--- PrismDemo/ViewModels/ViewAViewModel.cs
+++ PrismDemo/ViewModels/ViewAViewModel.cs
@@ -1,4 +1,5 @@
-﻿using Prism.Mvvm;
+﻿using Prism.Commands;
+using Prism.Mvvm;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -23,7 +24,8 @@ namespace PrismDemo.ViewModels
         public string LastName
         {
             get { return _lastName; }
-            set { SetProperty(ref _lastName, value); }
+            set { SetProperty(ref _lastName, value);
+                UpdateCommand.RaiseCanExecuteChanged(); }
         }
 
         private DateTime? _lastUpdated;
@@ -31,8 +33,25 @@ namespace PrismDemo.ViewModels
         public DateTime? LastUpdated
         {
             get { return _lastUpdated; }
-            set { SetProperty(ref _lastUpdated, value); }
+            set { SetProperty(ref _lastUpdated, value); 
+                UpdateCommand.RaiseCanExecuteChanged(); }
         }
 
+        public DelegateCommand UpdateCommand { get; set; }
+
+        public ViewAViewModel()
+        {
+            UpdateCommand = new DelegateCommand(Execute, CanExecute);
+        }
+
+        private bool CanExecute()
+        {
+            return !String.IsNullOrWhiteSpace(FirstName) && !String.IsNullOrWhiteSpace(LastName);
+        }
+
+        private void Execute()
+        {
+            LastUpdated = DateTime.Now;
+        }
     }
 }
