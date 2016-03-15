a4f9db4b261afe850c9a17f6cb485a069d1892cb
petercharlesxp <petercharles.ca@hotmail.com>
2016-03-15 15:14:52 -0400

    Set DataContext in ViewA.xaml.cs

---
+++ PrismDemo/ViewModels/ViewAViewModel.cs
@@ -0,0 +1,38 @@
+﻿using Prism.Mvvm;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace PrismDemo.ViewModels
+{
+    public class ViewAViewModel: BindableBase
+    {
+        private string _firstName = "Brian";
+
+        public string FirstName
+        {
+            get { return _firstName; }
+            set { SetProperty(ref _firstName, value); }
+        }
+
+        private string _lastName;
+
+        public string LastName
+        {
+            get { return _lastName; }
+            set { SetProperty(ref _lastName, value); }
+        }
+
+        private DateTime? _lastUpdated;
+
+        public DateTime? LastUpdated
+        {
+            get { return _lastUpdated; }
+            set { SetProperty(ref _lastUpdated, value); }
+        }
+
+    }
+}
